85cc6d27466d1ccf8fbae04e6ec46d7f6f83fce7
pftom <huan1043269994@gmail.com>
2018-05-26 15:25:36 +0800

    refactor renderExplain in Content and ContentItem

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,31 +8,33 @@ import './css/Content.css';
 
 
 export default class Content extends Component {
+  renderExplain = (explain) => {
+    return (
+      isArray(explain)
+      ? (explain.map((explainItem, i) => <p key={i}>{explainItem}</p>))
+      : <p>{explain}</p>
+    );
+  }
+
   render() {
     const { name, explain, diff, commit } = this.props.content;
     return (
       <div className="Content">
         <div className="Content-header">
           <h1>{name}</h1>
-          <Button 
-            type="primary" 
+          <Button
+            type="primary"
             onClick={this.props.changeViewType}
           >
             {this.props.viewType}
           </Button>
         </div>
-        {
-          isArray(explain) 
-          ? (
-            explain.map((explainItem, i) => <p key={i}>{explainItem}</p>)
-          ) : (
-            <p>{explain}</p>
-          )
-        }
-        <ContentItem 
-          diff={diff} 
+        {this.renderExplain(explain)}
+        <ContentItem
+          diff={diff}
           commit={commit}
           viewType={this.props.viewType}
+          renderExplain={this.renderExplain}
         />
       </div>
     );
