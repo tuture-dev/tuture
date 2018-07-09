025d93ca539bd52cf7a539f4c591d5cdb8cd80fd
pftom <huan1043269994@gmail.com>
2018-07-09 11:22:25 +0800

    Delete related collapse content as it is not need

--- src/components/StepDiff.tsx
+++ src/components/StepDiff.tsx
@@ -18,10 +18,6 @@ interface StepDiffProps {
   renderExplain: RenderExplainFunc;
 }
 
-interface CollapseObj {
-  [index: string]: boolean;
-}
-
 interface ResObj {
   [index: string]: ChangedFile[] | File[];
 }
@@ -52,8 +48,8 @@ export default class StepDiff extends React.PureComponent<StepDiffProps> {
     // use fileName key map it belongs obj
     const mapedFiles = this.mapArrItemToObjValue('newPath', files);
     const mapedDiff = this.mapArrItemToObjValue('file', diff);
-    const endRenderContent = diff.map((diffItem) => {
-      const fileName = diffItem.file;
+    const endRenderContent = diff.map((item) => {
+      const fileName = item.file;
       return {
         ...mapedDiff[fileName],
         ...mapedFiles[fileName],
@@ -63,52 +59,35 @@ export default class StepDiff extends React.PureComponent<StepDiffProps> {
     return endRenderContent;
   };
 
-  getCollapseObj = (arr: ChangedFile[]): CollapseObj => {
-    // write a new func for reduce repeat work
-    const constructNewCollapseObj: CollapseObj = {};
-    if (tutureUtilities.isArray(arr)) {
-      arr.map((arrItem) => {
-        constructNewCollapseObj[arrItem.file] = arrItem.collapse;
-      });
-    }
-
-    return constructNewCollapseObj;
-  };
-
   render() {
     const { diff, diffItem } = this.props;
-    const collapseObj = this.getCollapseObj(diff);
     const needRenderFiles = this.getEndRenderContent(diff, diffItem.diff);
 
     return (
       <div className="ContentItem">
-        {needRenderFiles.map(
-          (file: File & ChangedFile, i) =>
-            file &&
-            !collapseObj[file.file] && (
-              <div key={i}>
-                <article className="diff-file" key={i}>
-                  <header className="diff-file-header">
-                    <strong className="filename">
-                      {this.extractFileName(file)}
-                    </strong>
-                  </header>
-                  <main>
-                    <DiffView
-                      key={i}
-                      hunks={file.hunks}
-                      viewType={this.props.viewType}
-                    />
-                  </main>
-                </article>
-                <div>
-                  <div style={{ marginTop: '20px' }}>
-                    {this.props.renderExplain(file.explain)}
-                  </div>
-                </div>
+        {needRenderFiles.map((file: File & ChangedFile, i) => (
+          <div key={i}>
+            <article className="diff-file" key={i}>
+              <header className="diff-file-header">
+                <strong className="filename">
+                  {this.extractFileName(file)}
+                </strong>
+              </header>
+              <main>
+                <DiffView
+                  key={i}
+                  hunks={file.hunks}
+                  viewType={this.props.viewType}
+                />
+              </main>
+            </article>
+            <div>
+              <div style={{ marginTop: '20px' }}>
+                {this.props.renderExplain(file.explain)}
               </div>
-            ),
-        )}
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
