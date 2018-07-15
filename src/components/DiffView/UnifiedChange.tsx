a0ee0a7072d250d86d45e11f74294ba11316d228
pftom <huan1043269994@gmail.com>
2018-07-15 21:17:31 +0800

    fix: delete redundant SplitChange property

--- src/components/DiffView/UnifiedChange.tsx
+++ src/components/DiffView/UnifiedChange.tsx
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react';
 import classnames from 'classnames';
 
 import { Change as ChangeType } from '../../types';
-import { Cell } from './SplitChange';
 import Snippet from './Snippet';
 
 import { computeOldLineNumber, computeNewLineNumber } from './utils';
@@ -12,27 +11,6 @@ interface UnifiedChangeProps {
 }
 
 export default class UnifiedChange extends PureComponent<UnifiedChangeProps> {
-  renderGutterCell = ({
-    hide,
-    className,
-    lineNumber,
-    gutterAnchor,
-    anchorID,
-    ...props
-  }: Cell): React.ReactNode => {
-    if (hide) {
-      return null;
-    }
-
-    return (
-      <td className={className} data-line-number={lineNumber} {...props}>
-        {gutterAnchor ? (
-          <a href={`#${anchorID}`} data-line-number={lineNumber} />
-        ) : null}
-      </td>
-    );
-  };
-
   render() {
     const { change } = this.props;
 
@@ -47,14 +25,6 @@ export default class UnifiedChange extends PureComponent<UnifiedChangeProps> {
 
     return (
       <tr className={classnames('diff-line')}>
-        {this.renderGutterCell({
-          className: gutterClassName,
-          lineNumber: oldLineNumber,
-        })}
-        {this.renderGutterCell({
-          className: gutterClassName,
-          lineNumber: newLineNumber,
-        })}
         <td className={codeClassName}>
           <Snippet code={content} />
         </td>
