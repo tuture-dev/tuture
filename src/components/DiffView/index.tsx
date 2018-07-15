7763a6f068e246b9ffeb12d9bf402b05d7362d92
pftom <huan1043269994@gmail.com>
2018-07-15 15:54:29 +0800

    refactor: delete diffview code block new/old line number

--- src/components/DiffView/index.tsx
+++ src/components/DiffView/index.tsx
@@ -13,24 +13,8 @@ export default class DiffView extends Component<DiffViewProps> {
   render() {
     const { viewType, hunks } = this.props;
 
-    const gutterContent =
-      viewType === 'Unified' ? (
-        <colgroup>
-          <col className="diff-gutter-col" />
-          <col className="diff-gutter-col" />
-        </colgroup>
-      ) : (
-        <colgroup>
-          <col className="diff-gutter-col" />
-          <col />
-          <col className="diff-gutter-col" />
-          <col />
-        </colgroup>
-      );
-
     return (
       <table className="diff">
-        {gutterContent}
         {hunks.map((hunk: HunkType, key: number) => (
           <Hunk key={key} viewType={viewType} hunk={hunk} />
         ))}
