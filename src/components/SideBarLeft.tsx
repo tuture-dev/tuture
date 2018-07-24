7b6b4354ec1b547dd9b2d3a95b91580a82c952ef
pftom <huan1043269994@gmail.com>
2018-07-24 16:52:59 +0800

    refactor: modify SideBarRightWrapper naming

--- src/components/SideBarLeft.tsx
+++ src/components/SideBarLeft.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import StepList, { StepListProps } from './StepList';
 
 /* tslint:disable-next-line */
-const SideBarRightWrapper = styled.div`
+const SideBarLeftWrapper = styled.div`
   width: 220px;
   background-color: rgba(216, 216, 216, 0.1);
   height: 637px;
@@ -16,13 +16,13 @@ export default class SideBarLeft extends React.Component<StepListProps> {
   render() {
     const { commits, selectKey, updateSelect } = this.props;
     return (
-      <SideBarRightWrapper>
+      <SideBarLeftWrapper>
         <StepList
           commits={commits}
           selectKey={selectKey}
           updateSelect={updateSelect}
         />
-      </SideBarRightWrapper>
+      </SideBarLeftWrapper>
     );
   }
 }
