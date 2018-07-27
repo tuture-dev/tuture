9baed9bcc9523d26f2a89c6448bba697df87e7c6
Tom Huang <1043269994@qq.com>
2018-07-27 20:50:06 +0800

    refactor: remove code emphasize when code file is newly added (#83)

--- src/components/DiffView.tsx
+++ src/components/DiffView.tsx
@@ -112,50 +112,31 @@ injectGlobal`
 `;
 
 export default class DiffView extends Component<DiffViewProps> {
-  getChangeKey(change: ChangeType): string {
-    if (!change) {
-      throw new Error('change is not provided');
-    }
-
-    const { isNormal, isInsert, lineNumber, oldLineNumber } = change;
-
-    if (isNormal) {
-      return 'N' + oldLineNumber;
-    }
-
-    const prefix = isInsert ? 'I' : 'D';
-    return prefix + lineNumber;
-  }
-
-  groupElements = (changes: ChangeType[]) =>
-    changes.reduce(
-      (elements: (string | ChangeType)[][], change: ChangeType) => {
-        const key = this.getChangeKey(change);
-
-        elements.push(['change', key, change]);
-
-        // later will add widget content
-        return elements;
-      },
-      [],
+  renderRow = (change: ChangeType, isAllInsert: Boolean, key: number) => {
+    const { type, content } = change;
+    const codeClassName = classnames('diff-code', {
+      [`diff-code-${type}`]: !isAllInsert,
+    });
+    return (
+      <tr key={`change${key}`} className={classnames('diff-line')}>
+        <td className={codeClassName}>
+          <Snippet code={content} />
+        </td>
+      </tr>
     );
+  };
 
-  renderRow = ([type, key, value]: (string | ChangeType)[], i: number) => {
-    if (type === 'change') {
-      const change = value as ChangeType;
-      const { type, content } = change;
-      const codeClassName = classnames('diff-code', `diff-code-${type}`);
-      return (
-        <tr key={`change${key}`} className={classnames('diff-line')}>
-          <td className={codeClassName}>
-            <Snippet code={content} />
-          </td>
-        </tr>
-      );
-    }
-
-    // later will add widget content
-    return null;
+  judgeAllRowInsertState = (changes: ChangeType[]) => {
+    let isAllInsert = true;
+    changes.map((change: ChangeType) => {
+      const { type } = change;
+      if (type !== 'insert') {
+        isAllInsert = false;
+      }
+    });
+    return changes.map((change, i) => {
+      return this.renderRow(change, isAllInsert, i);
+    });
   };
 
   render() {
@@ -165,9 +146,7 @@ export default class DiffView extends Component<DiffViewProps> {
       <table className="diff">
         {hunks.map((hunk: HunkType, key: number) => (
           <tbody key={key} className={classnames('diff-hunk')}>
-            {this.groupElements(hunk.changes).map((element, i) =>
-              this.renderRow(element, i),
-            )}
+            {this.judgeAllRowInsertState(hunk.changes)}
           </tbody>
         ))}
       </table>
