018c22bd8d8d3aea29b03c78c748ad7d9cebea39
mRcfps <1402491442@qq.com>
2018-07-31 11:09:42 +0800

    Improve dev and build workflow
    * start: use concurrently to build and start server concurrently
    * build: two scripts merged into one

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
 
-module.exports = (env, argv) => {
-  const base = {
-    entry: './src/server/index.ts',
-    output: {
-      filename: 'js/server.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-    },
-    devtool: 'cheap-module-eval-source-map',
-    resolve: {
-      extensions: ['.js', '.json', '.ts', '.tsx'],
-    },
-    module: {
-      strictExportPresence: true,
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: ['file-loader'],
-        },
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-        },
-      ],
-    },
-  };
-
-  if (env.platform === 'server') {
-    base.target = 'node';
-  }
-
-  if (env.platform === 'web') {
-    base.entry = './src/index.tsx';
-    base.output.filename = 'js/client.js';
-  }
-
-  return base;
+const base = {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
 };
+
+const serverConfig = {
+  target: 'node',
+  entry: './src/server/index.ts',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist', 'js'),
+  },
+};
+
+const clientConfig = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'dist', 'js'),
+  },
+};
+
+module.exports = [
+  Object.assign(serverConfig, base),
+  Object.assign(clientConfig, base),
+];
