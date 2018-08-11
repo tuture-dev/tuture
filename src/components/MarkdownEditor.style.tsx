e2f681b4e6ee047db85106f45551fda09a2d56ce
pftom <huan1043269994@gmail.com>
2018-08-11 12:50:46 +0800

    Refactor ExplainedItem

---
+++ src/components/MarkdownEditor.style.tsx
@@ -0,0 +1,246 @@
+import styled, { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  .markdown p, li {
+    font-family: Georgia;
+    font-size: 18px;
+    line-height: 1.58;
+    margin: 24px 0 0 0;
+    color: rgba(0,0,0,0.84);
+  }
+
+  .markdown li {
+    margin: 8px 0;
+  }
+
+  .markdown h1 {
+    font-size: 37px;
+    font-family: LucidaGrande-Bold;
+  }
+
+  .markdown h2 {
+    font-size: 31px;
+    font-family: LucidaGrande-Bold;
+  }
+
+  .markdown h3 {
+    font-size: 26px;
+    font-family: LucidaGrande-Bold;
+  }
+
+  .markdown pre {
+    font-family: Monaco,Menlo,"Courier New",Courier,monospace;
+    font-size: 14px;
+    font-weight: 400;
+    color: rgba(0,0,0,0.66);
+    display: block;
+    padding: 20px;
+    background-color: rgba(0, 0, 0, .05);
+    margin: 32px 0 !important;
+  }
+
+  .markdown a {
+    color: rgba(0,0,0,0.84);
+    text-decoration: none;
+    border-bottom: 1px solid rgba(0,0,0,0.84);
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .markdown img {
+    display: block;
+    width: 680px;
+    margin: 44px 0;
+  }
+
+  .markdown :not(pre) > code {
+    font-family: Monaco,Monaco,"Courier New",Courier,monospace;
+    background-color: rgba(0, 0, 0, .05);
+    padding: 3px 4px;
+    margin: 0 2px;
+    font-size: 14px;
+  }
+
+  .markdown blockquote {
+    font-style: italic;
+    margin-top: 28px;
+    border-left: 3px solid rgba(0,0,0,.84);
+    padding-left: 20px;
+    margin-left: -21px;
+    padding-bottom: 2px;
+  }
+
+  .markdown blockquote p {
+    margin: 0;
+    line-height: 1.58;
+  }
+
+  .markdown blockquote :not(pre) > code {
+    font-style: normal;
+  }
+
+  .markdown.preview-markdown {
+    box-sizing: border-box;
+    padding: 20px;
+    border: 1px solid #d1d5da;
+    border-radius: 0 4px 4px;
+  }
+
+  .markdown.preview-markdown p:first-child {
+    margin-top: 0;
+  }
+
+  .markdown.preview-markdown li:first-child {
+    margin-top: 0;
+  }
+
+  .markdown.preview-markdown h1:first-child {
+    margin-top: 0;
+  }
+
+  .markdown.preview-markdown h2:first-child {
+    margin-top: 0;
+  }
+
+  .is-root {
+    padding: 0 24px 12px;
+  }
+
+  .editor > .is-root {
+    padding-left: 0;
+    padding-right: 0;
+  }
+
+  .editor > .is-root.preview-markdown {
+    padding: 20px;
+  }
+
+  textarea {
+    display: block;
+    width: 100%;
+    height: auto;
+    overflow-y: hidden;
+    vertical-align: top;
+    box-sizing: border-box;
+    font-family: Georgia;
+    padding: 20px;
+    resize: none;
+    font-size: 18px;
+    border: 1px solid #d1d5da;
+    border-radius: 0 4px 4px;
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const TabWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BasicButton = styled.button`
+  height: 30px;
+  line-height: 30px;
+  padding: 0 18px;
+  font-size: 12px;
+  box-sizing: border-box;
+  position: relative;
+  background-color: white;
+  outline: none;
+  &:hover {
+    outline: none;
+    cursor: pointer;
+  }
+`;
+
+const Button = styled(BasicButton)`
+  border: ${(props: { selected?: boolean; color?: string }) =>
+    props.color
+      ? `1px solid ${props.color}`
+      : props.selected
+        ? '1px solid #d1d5da;'
+        : '1px solid transparent'};
+  border-bottom: ${(props: { selected?: boolean; color?: string }) =>
+    props.color ? `1px solid ${props.color}` : props.selected && '0'};
+  border-radius: ${(props: { selected?: boolean; color?: string }) =>
+    props.color ? '4px' : '3px 3px 0 0'};
+  color: ${(props: { selected?: boolean; color?: string }) =>
+    props.color
+      ? props.color
+      : props.selected
+        ? 'rgba(0,0,0,.84)'
+        : 'rgba(0,0,0,.84)'};
+  bottom: ${(props: { selected?: boolean; color?: string }) =>
+    props.selected ? '-1px' : 0};
+`;
+
+const SaveButton = styled(BasicButton)`
+  color: #00b887;
+  border: none;
+`;
+
+const ToolButton = styled.button`
+  margin-right: 10px;
+`;
+
+const HasExplainWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  &:hover {
+    opacity: 1;
+    transition: opacity 0.3s;
+    border: 1px solid #00b887;
+  }
+`;
+
+const EditExplainWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const HasExplainButton = styled(BasicButton)`
+  color: ${(props: { color: string; border: string }) => props.color};
+  border: ${(props: { color: string; border: string }) => props.border};
+  border-radius: 4px;
+  margin-right: 30px;
+`;
+
+const NoExplainWrapper = styled.div`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #00b887;
+  color: #00b887;
+  padding: 10px;
+  font-size: 20px;
+  opacity: 0;
+  border-radius: 3px;
+  text-align: center;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export {
+  TabWrapper,
+  BasicButton,
+  Button,
+  SaveButton,
+  ToolButton,
+  HasExplainWrapper,
+  EditExplainWrapper,
+  HasExplainButton,
+  NoExplainWrapper,
+};
---
+++ src/components/MarkdownTool.tsx
@@ -0,0 +1,353 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// @ts-ignore
+import Upload from 'rc-upload';
+
+import { ToolButton } from './MarkdownEditor.style';
+import { ToolType } from '../types/ExplainedItem';
+
+export interface MarkdownToolProps {
+  source: string;
+  explainContentRef: React.RefObject<HTMLTextAreaElement>;
+  edit: boolean;
+  changeState: (content: string) => void;
+  changePosition: (position: number) => void;
+}
+
+const MarkdownToolWrapper = styled.div`
+  border: 1px solid #d1d5da;
+  border-bottom: none;
+  height: 30px;
+  padding: 5px 10px;
+  line-height: 30px;
+`;
+
+export default class MarkdownTool extends React.Component<MarkdownToolProps> {
+  spliceStr = (str = '', typeStr: string, start: number, end: number) => {
+    return str
+      .slice(0, start)
+      .concat(
+        typeStr,
+        str.slice(start, end),
+        typeStr,
+        str.slice(end, str.length),
+      );
+  };
+
+  isAtBeginning = (explainContent: string, selectedStart: number) =>
+    selectedStart === 0 ||
+    explainContent.slice(0, selectedStart).endsWith('\n');
+
+  isAtEnding = (explainContent: string, selectedEnd: number) =>
+    selectedEnd === explainContent.length ||
+    explainContent.slice(selectedEnd, explainContent.length).indexOf('\n') ===
+      0;
+
+  changeState = (source: string, position?: number) => {
+    const { changeState, changePosition } = this.props;
+    changeState(source);
+
+    if (position) {
+      changePosition(position);
+    }
+  };
+
+  handleMdTool = (toolType: ToolType) => {
+    const { edit, source, explainContentRef } = this.props;
+    const explainTextarea = explainContentRef.current;
+    if (edit) {
+      const explainContent = source || '';
+      console.log(explainTextarea);
+      const selectedContent = explainContent.slice(
+        explainTextarea.selectionStart,
+        explainTextarea.selectionEnd,
+      );
+      let resultContent = '';
+      switch (toolType) {
+        case 'b': {
+          if (selectedContent) {
+            const source = this.spliceStr(
+              explainContent,
+              '**',
+              explainTextarea.selectionStart,
+              explainTextarea.selectionEnd,
+            );
+            this.changeState(
+              source,
+              explainTextarea.selectionStart + selectedContent.length + 2,
+            );
+          } else {
+            this.changeState(
+              `${explainContent}****`,
+              explainContent.length + 2,
+            );
+          }
+          break;
+        }
+        case 'i':
+          if (selectedContent) {
+            const source = this.spliceStr(
+              explainContent,
+              '_',
+              explainTextarea.selectionStart,
+              explainTextarea.selectionEnd,
+            );
+            this.changeState(
+              source,
+              explainTextarea.selectionStart + selectedContent.length + 1,
+            );
+          } else {
+            this.changeState(`${explainContent}__`, explainContent.length + 1);
+          }
+          break;
+        case 'h': {
+          !explainContent || explainContent.endsWith('\n')
+            ? this.changeState(`${explainContent}#### `, 0)
+            : this.changeState(`${explainContent}\n#### `, 0);
+          break;
+        }
+        case 'list':
+          if (selectedContent) {
+            let resultContent: string = '';
+            const listItems = selectedContent.split('\n');
+            listItems.map((item) => {
+              if (item) {
+                resultContent += `- ${item}\n`;
+              }
+            });
+            const source = explainContent
+              .slice(0, explainTextarea.selectionStart)
+              .concat(
+                this.isAtBeginning(
+                  explainContent,
+                  explainTextarea.selectionStart,
+                )
+                  ? ''
+                  : '\n',
+                resultContent,
+                this.isAtEnding(explainContent, explainTextarea.selectionEnd)
+                  ? ''
+                  : '\n',
+                explainContent.slice(
+                  explainTextarea.selectionEnd,
+                  explainContent.length,
+                ),
+              );
+            this.changeState(source);
+          } else {
+            !explainContent || explainContent.endsWith('\n')
+              ? this.changeState(`${explainContent}- `, 0)
+              : this.changeState(`${explainContent}\n- `, 0);
+          }
+          break;
+        case 'numbered list':
+          if (selectedContent) {
+            let resultContent: string = '';
+            const listItems = selectedContent.split('\n');
+            listItems.map((item, index) => {
+              if (item) {
+                resultContent += `${index + 1}. ${item}\n`;
+              }
+            });
+            const source = explainContent
+              .slice(0, explainTextarea.selectionStart)
+              .concat(
+                this.isAtBeginning(
+                  explainContent,
+                  explainTextarea.selectionStart,
+                )
+                  ? ''
+                  : '\n\n',
+                resultContent,
+                this.isAtEnding(explainContent, explainTextarea.selectionEnd)
+                  ? ''
+                  : '\n',
+                explainContent.slice(
+                  explainTextarea.selectionEnd,
+                  explainContent.length,
+                ),
+              );
+            this.changeState(source);
+          } else {
+            !explainContent || explainContent.endsWith('\n')
+              ? this.changeState(`${explainContent}1. `, 0)
+              : this.changeState(`${explainContent}\n1. `, 0);
+          }
+          break;
+        case 'blockquotes':
+          if (selectedContent) {
+            let resultContent: string;
+            if (
+              explainTextarea.selectionStart === 0 ||
+              selectedContent.indexOf('\n') === 0
+            ) {
+              resultContent = '';
+            } else {
+              resultContent = '\n';
+            }
+            const listItems = selectedContent.split('\n');
+            listItems.map((item, index) => {
+              if (item) {
+                resultContent += `> ${item}\n`;
+              }
+            });
+            const source = explainContent
+              .slice(0, explainTextarea.selectionStart)
+              .concat(
+                this.isAtBeginning(
+                  explainContent,
+                  explainTextarea.selectionStart,
+                )
+                  ? ''
+                  : '\n',
+                resultContent,
+                this.isAtEnding(explainContent, explainTextarea.selectionEnd)
+                  ? ''
+                  : '\n',
+                explainContent.slice(
+                  explainTextarea.selectionEnd,
+                  explainContent.length,
+                ),
+              );
+            this.changeState(source);
+          } else {
+            !explainContent || explainContent.endsWith('\n')
+              ? this.changeState(`${explainContent}> `, 0)
+              : this.changeState(`${explainContent}\n> `, 0);
+          }
+
+          break;
+        case 'code':
+          if (selectedContent) {
+            const source = this.spliceStr(
+              explainContent,
+              '`',
+              explainTextarea.selectionStart,
+              explainTextarea.selectionEnd,
+            );
+            this.changeState(
+              source,
+              explainTextarea.selectionStart + selectedContent.length + 1,
+            );
+          } else {
+            this.changeState(explainContent + '``', explainContent.length + 1);
+          }
+          break;
+        case 'block code':
+          if (selectedContent) {
+            const source = this.spliceStr(
+              explainContent,
+              '\n```\n',
+              explainTextarea.selectionStart,
+              explainTextarea.selectionEnd,
+            );
+            this.changeState(
+              source,
+              explainTextarea.selectionStart + selectedContent.length + 5,
+            );
+          } else {
+            if (!explainContent || explainContent.endsWith('\n')) {
+              this.changeState(
+                explainContent + '```\n\n```',
+                explainContent.length + 4,
+              );
+            } else {
+              this.changeState(
+                explainContent + '\n```\n\n```',
+                explainContent.length + 5,
+              );
+            }
+          }
+          break;
+        case 'link':
+          if (selectedContent) {
+            resultContent = `${explainContent.slice(
+              0,
+              explainTextarea.selectionStart,
+            )}[${selectedContent}]()${explainContent.slice(
+              explainTextarea.selectionEnd,
+              explainContent.length,
+            )}`;
+            this.changeState(
+              resultContent,
+              explainTextarea.selectionStart + selectedContent.length + 3,
+            );
+          } else {
+            this.changeState(
+              `${explainContent}[](url)`,
+              explainContent.length + 1,
+            );
+          }
+
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  render() {
+    const toolArr = [
+      {
+        type: 'b',
+        value: 'B',
+        children: <b>B</b>,
+      },
+      {
+        type: 'h',
+        value: 'H',
+      },
+      {
+        type: 'i',
+        value: 'I',
+        children: <i>I</i>,
+      },
+      {
+        type: 'list',
+        value: 'List',
+      },
+      {
+        type: 'numbered list',
+        value: 'Numbered List',
+      },
+      {
+        type: 'blockquotes',
+        value: 'Blockquotes',
+      },
+      {
+        type: 'code',
+        value: 'Code',
+      },
+      {
+        type: 'block code',
+        value: 'Block Code',
+      },
+      {
+        type: 'link',
+        value: 'Link',
+      },
+    ];
+    return (
+      <MarkdownToolWrapper>
+        {toolArr.map((tool, index) => (
+          <ToolButton
+            key={index}
+            onClick={() => this.handleMdTool(tool.type as ToolType)}>
+            {tool.children ? tool.children : tool.value}
+          </ToolButton>
+        ))}
+        <Upload
+          name="file"
+          action="http://localhost:3000/upload"
+          accept=".jpg,.jpeg,.png,.gif"
+          onSuccess={(body: { path: string }) => {
+            const explainContent = this.props.source;
+            this.changeState(explainContent + `![](${body.path})`);
+          }}>
+          <ToolButton>Img</ToolButton>
+        </Upload>
+      </MarkdownToolWrapper>
+    );
+  }
+}
