b151dc8a2661c5ba04fb2be66009bbd4e93a9c2c
pftom <huan1043269994@gmail.com>
2018-09-12 10:34:34 +0800

    Make client and server share same resources

--- src/i18n.server.ts
+++ src/i18n.server.ts
@@ -2,47 +2,49 @@ import i18n from 'i18next';
 import Backend from 'i18next-node-fs-backend';
 import { LanguageDetector } from 'i18next-express-middleware';
 
+export const resources = {
+  en: {
+    translations: {
+      tagTitle: 'This tutorial contains:',
+      deleteButton: 'Delete',
+      editButton: 'Edit',
+      editTab: 'Edit',
+      previewTab: 'Preview',
+      cancelButton: 'Cancel',
+      saveButton: 'Save',
+      fillStepIntroduction: 'Fill the introduction of this step',
+      fillStepExplain: 'Fill the explain of this step',
+      fillFileIntrodution: 'Fill the introduction of this file',
+      fillFileExplain: 'Fill the explain of this file',
+      menu: 'Menu',
+      copySuccess: 'Copied',
+    },
+  },
+  'zh-CN': {
+    translations: {
+      tagTitle: '本篇教程涉及的内容：',
+      deleteButton: '删除',
+      editButton: '编辑',
+      editTab: '编辑',
+      previewTab: '预览',
+      cancelButton: '取消',
+      saveButton: '保存',
+      fillStepIntroduction: '填写此步骤的介绍文字',
+      fillStepExplain: '填写此步骤的解释文字',
+      fillFileIntrodution: '填写此文件的介绍文字',
+      fillFileExplain: '填写此文件的解释文字',
+      menu: '教程目录',
+      copySuccess: '复制成功',
+    },
+  },
+};
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .init({
     // we init with resources
-    resources: {
-      en: {
-        translations: {
-          tagTitle: 'This tutorial contains:',
-          deleteButton: 'Delete',
-          editButton: 'Edit',
-          editTab: 'Edit',
-          previewTab: 'Preview',
-          cancelButton: 'Cancel',
-          saveButton: 'Save',
-          fillStepIntroduction: 'Fill the introduction of this step',
-          fillStepExplain: 'Fill the explain of this step',
-          fillFileIntrodution: 'Fill the introduction of this file',
-          fillFileExplain: 'Fill the explain of this file',
-          menu: 'Menu',
-          copySuccess: 'Copied',
-        },
-      },
-      'zh-CN': {
-        translations: {
-          tagTitle: '本篇教程涉及的内容：',
-          deleteButton: '删除',
-          editButton: '编辑',
-          editTab: '编辑',
-          previewTab: '预览',
-          cancelButton: '取消',
-          saveButton: '保存',
-          fillStepIntroduction: '填写此步骤的介绍文字',
-          fillStepExplain: '填写此步骤的解释文字',
-          fillFileIntrodution: '填写此文件的介绍文字',
-          fillFileExplain: '填写此文件的解释文字',
-          menu: '教程目录',
-          copySuccess: '复制成功',
-        },
-      },
-    },
+    resources,
     fallbackLng: 'en',
     debug: true,
 
