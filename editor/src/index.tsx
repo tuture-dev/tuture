7a2843110085a44399b86daf55f224ac2bba04a8
mRcfps <1402491442@qq.com>
2018-09-26 19:54:08 +0800

    Remove unused dependencies in editor

--- editor/src/index.tsx
+++ editor/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
-import yaml from 'js-yaml';
 import { I18nextProvider } from 'react-i18next';
 
 import i18n from './i18n';
@@ -16,14 +15,14 @@ const fetchTasks = [fetch('/diff'), fetch('/tuture')];
 Promise.all(fetchTasks)
   .then((responses) => {
     const [diffRes, tutureRes] = responses;
-    return Promise.all([diffRes.json(), tutureRes.text()]);
+    return Promise.all([diffRes.json(), tutureRes.json()]);
   })
   .then((data) => {
     const [diff, tuture] = data;
     ReactDOM.render(
       <I18nextProvider i18n={i18n}>
         <Provider store={store}>
-          <App tuture={yaml.safeLoad(tuture)} diff={diff} />
+          <App tuture={tuture} diff={diff} />
         </Provider>
       </I18nextProvider>,
       document.getElementById('root'),
