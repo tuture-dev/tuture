3af79021b11a86b70c74a26d804b40dd5ef508f4
mRcfps <1402491442@qq.com>
2018-10-10 16:41:36 +0800

    Keep exe suffix  for windows packing

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -18,9 +18,19 @@ make_tarball() {
   rm tuture
 }
 
+make_win_tarball() {
+  binary=$1
+  tarball_name=$2
+
+  # we have to keep .exe suffix here.
+  mv $binary tuture.exe
+  tar -czvf dist/$tarball_name tuture.exe
+  rm tuture.exe
+}
+
 make_tarball tuture-linux-x64 tuture-linux-x64.tar.gz
 make_tarball tuture-linux-x86 tuture-linux-x86.tar.gz
 make_tarball tuture-macos-x64 tuture-macos-x64.tar.gz
 make_tarball tuture-macos-x86 tuture-macos-x86.tar.gz
-make_tarball tuture-win-x64.exe tuture-win-x64.tar.gz
-make_tarball tuture-win-x86.exe tuture-win-x86.tar.gz
+make_win_tarball tuture-win-x64.exe tuture-win-x64.tar.gz
+make_win_tarball tuture-win-x86.exe tuture-win-x86.tar.gz
