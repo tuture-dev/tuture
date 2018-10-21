63243faa385dabbc91df923d41519e7e381d4bb8
mRcfps <1402491442@qq.com>
2018-10-21 22:27:19 +0800

    Move editor-related path to config

--- server.ts
+++ server.ts
@@ -7,7 +7,12 @@ import multer from 'multer';
 import express from 'express';
 import socketio from 'socket.io';
 
-import { TUTURE_YML_PATH, DIFF_PATH } from './config';
+import {
+  TUTURE_YML_PATH,
+  DIFF_PATH,
+  EDITOR_STATIC_PATH,
+  EDITOR_PATH,
+} from './config';
 
 const app = express();
 const server = http.createServer(app);
@@ -35,13 +40,11 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/static', express.static(path.join(__dirname, './editor/static')));
+app.use('/static', express.static(EDITOR_STATIC_PATH));
 app.use('/tuture-assets', express.static(assetsPath));
 
 app.get('/', (_, res) => {
-  const html = fs
-    .readFileSync(path.join(__dirname, './editor/index.html'))
-    .toString();
+  const html = fs.readFileSync(path.join(EDITOR_PATH, 'index.html')).toString();
   res.send(html);
 });
 
