454499e809d9b050fcc8c19c4ca15cc9af6dd8b0
mRcfps <1402491442@qq.com>
2019-10-27 22:08:02 +0800

    Remove error logging

--- constants.ts
+++ constants.ts
@@ -4,8 +4,6 @@ export const TUTURE_YML_PATH = 'tuture.yml';
 
 export const TUTURE_IGNORE_PATH = '.tutureignore';
 
-export const TUTURE_ERROR_LOG = 'tuture-error.log';
-
 // Directory which houses tuture internal files.
 export const TUTURE_ROOT = '.tuture';
 
--- utils/logger.ts
+++ utils/logger.ts
@@ -1,6 +1,5 @@
 import chalk from 'chalk';
 import winston, { format } from 'winston';
-import { TUTURE_ERROR_LOG } from '../constants';
 
 const logLevels = {
   error: 0,
@@ -39,11 +38,6 @@ const logger = winston.createLogger({
   levels: logLevels,
   transports: [
     new winston.transports.Console({ level: 'info', format: consoleFormat }),
-    new winston.transports.File({
-      filename: TUTURE_ERROR_LOG,
-      level: 'error',
-      format: format.combine(format.timestamp(), fileFormat),
-    }),
   ],
 });
 
