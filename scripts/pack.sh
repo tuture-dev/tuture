d84fe0cf762b37e41653ce591bdc5aa4595d87d1
mRcfps <1402491442@qq.com>
2019-11-08 19:26:42 +0800

    Update release script and add workflow

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -7,30 +7,27 @@ rm -rf dist
 mkdir dist
 
 # packaging binaries with pkg.
-npx pkg -t node8-linux-x64,node8-linux-x86,node8-macos-x64,node8-macos-x86,node8-win-x64,node8-win-x86 .
+npx pkg -t node12-linux-x64,node12-macos-x64,node12-win-x64 .
 
-make_tarball() {
+make_zip() {
   binary=$1
-  tarball_name=$2
+  target=$2
 
   mv $binary tuture
-  tar -czvf dist/$tarball_name tuture
+  zip -r dist/$target tuture
   rm tuture
 }
 
-make_win_tarball() {
+make_win_zip() {
   binary=$1
-  tarball_name=$2
+  target=$2
 
   # we have to keep .exe suffix here.
   mv $binary tuture.exe
-  tar -czvf dist/$tarball_name tuture.exe
+  zip -r dist/$target tuture.exe
   rm tuture.exe
 }
 
-make_tarball tuture-linux-x64 tuture-linux-x64.tar.gz
-make_tarball tuture-linux-x86 tuture-linux-x86.tar.gz
-make_tarball tuture-macos-x64 tuture-macos-x64.tar.gz
-make_tarball tuture-macos-x86 tuture-macos-x86.tar.gz
-make_win_tarball tuture-win-x64.exe tuture-win-x64.tar.gz
-make_win_tarball tuture-win-x86.exe tuture-win-x86.tar.gz
+make_zip tuture-linux tuture-linux.zip
+make_zip tuture-macos tuture-macos.zip
+make_win_zip tuture-win.exe tuture-windows.zip
