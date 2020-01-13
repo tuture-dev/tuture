364f14dc9f6562f99353d17f033e5b8c38e0940e
Tom Huang <1043269994@qq.com>
2020-01-14 00:01:39 +0800

    Improve bodyParser limit to 50MB (#189)

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ const makeServer = (config: any) => {
     app.use(logger('dev'));
   }
 
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: false }));
   app.use('/static', express.static(EDITOR_STATIC_PATH));
   app.use(`/${assetsRoot}`, express.static(assetsRoot));
