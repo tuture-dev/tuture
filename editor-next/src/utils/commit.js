8d393a1eab927c3d4388c850c0ae5eae7609580a
mRcfps <1402491442@qq.com>
2020-02-27 12:11:53 +0800

    Improve commit comparison

--- editor-next/src/utils/commit.js
+++ editor-next/src/utils/commit.js
@@ -2,5 +2,8 @@ export function isCommitEqual(commit1, commit2) {
   if (!commit1 || !commit2) {
     return false;
   }
-  return commit1.startsWith(commit2) || commit2.startsWith(commit1);
+  return (
+    String(commit1).startsWith(String(commit2)) ||
+    String(commit2).startsWith(String(commit1))
+  );
 }
