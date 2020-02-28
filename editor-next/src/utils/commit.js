99e7e2946027af284b8b8f20aafbe8af4ed75e00
pftom <1043269994@qq.com>
2020-02-28 20:05:05 +0800

    Add timeout for submit

--- editor-next/src/utils/commit.js
+++ editor-next/src/utils/commit.js
@@ -7,3 +7,12 @@ export function isCommitEqual(commit1, commit2) {
     String(commit2).startsWith(String(commit1))
   );
 }
+
+export function timeout(ms, promise) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      reject(new Error('timeout'));
+    }, ms);
+    promise.then(resolve, reject);
+  });
+}
