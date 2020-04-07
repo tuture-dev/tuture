d265bb25a6d7eb511ff991f5439bcc049502d3ad
Tom Huang <huan1043269994@gmail.com>
2020-04-07 11:00:17 +0800

    Replace fetch with axios

--- editor/src/models/sync.tsx
+++ editor/src/models/sync.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { message, notification } from 'antd';
+import axios, { AxiosResponse } from 'axios';
 
 import { Dispatch } from '../store';
 import { Remote } from '../../../types';
@@ -30,53 +31,31 @@ export const sync = {
       await dispatch.collection.saveCollection();
 
       try {
-        const response = await fetch('/sync');
-        const { exitCode } = await response.json();
+        const response = await axios.get('/sync');
 
-        if (response.ok) {
+        if (response.status === 200) {
           if (payload?.showMessage) {
             message.success('同步内容成功！');
           }
-        } else {
-          switch (exitCode) {
-            case EXIT_CODE.CONFLICT: {
-              if (payload?.showMessage) {
-                notification.error({
-                  message: '内容同步可能发生了文件冲突',
-                  description: (
-                    <div>
-                      <p>
-                        请检查代码仓库是否存在冲突文件，并通过以下流程解决：
-                      </p>
-                      <p>1）打开发生冲突的文件，解决冲突；</p>
-                      <p>2）将冲突的文件添加到暂存区；</p>
-                      <p>
-                        3）运行 <code>tuture sync --continue</code> 继续同步。
-                      </p>
-                    </div>
-                  ),
-                  duration: null,
-                });
-              }
-
-              break;
-            }
-
-            default: {
-              if (payload?.showMessage) {
-                notification.error({
-                  message:
-                    mapExitCodeToMessage[exitCode as EXIT_CODE] ||
-                    '同步存在问题，请继续尝试',
-                });
-              }
-            }
-          }
         }
       } catch (err) {
-        console.log('err', err);
         if (payload?.showMessage) {
-          message.error('同步内容失败');
+          if (payload?.showMessage) {
+            notification.error({
+              message: '内容同步可能发生了文件冲突',
+              description: (
+                <div>
+                  <p>请检查代码仓库是否存在冲突文件，并通过以下流程解决：</p>
+                  <p>1）打开发生冲突的文件，解决冲突；</p>
+                  <p>2）将冲突的文件添加到暂存区；</p>
+                  <p>
+                    3）运行 <code>tuture sync --continue</code> 继续同步。
+                  </p>
+                </div>
+              ),
+              duration: null,
+            });
+          }
         }
       }
     },
