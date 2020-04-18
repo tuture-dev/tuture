6750a2a424129263c6b605e2b49c4aa05c984ede
mRcfps <1402491442@qq.com>
2020-04-18 22:18:36 +0800

    Remove dist files from local-server

--- packages/local-server/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './assets';
-export * from './collection';
-export * from './server';
-//# sourceMappingURL=index.d.ts.map
--- packages/local-server/dist/server.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const makeServer: () => import("express-serve-static-core").Express;
-//# sourceMappingURL=server.d.ts.map
