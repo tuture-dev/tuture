5b317adfd67d402da460b43c3fc3b58a538dca06
Marc Chan <mrc@mail.tuture.co>
2020-05-05 19:56:38 +0800

    Delete remote branch when running destroy command (#285)
    * Delete remote branch when running destroy command
    * Catch possible errors when deleting remote branches

--- packages/cli/src/commands/destroy.ts
+++ packages/cli/src/commands/destroy.ts
@@ -7,10 +7,6 @@ import logger from '../utils/logger';
 import { git } from '../utils/git';
 import { removeTutureSuite } from '../utils';
 
-type ConfirmResponse = {
-  answer: boolean;
-};
-
 export default class Destroy extends BaseCommand {
   static description = 'Delete all tuture files';
 
@@ -22,34 +18,62 @@ export default class Destroy extends BaseCommand {
     }),
   };
 
-  async promptConfirmDestroy() {
-    const response = await prompt([
+  async promptConfirm(message: string, defaultChoice = false) {
+    const response = await prompt<{ answer: boolean }>([
       {
         type: 'confirm',
         name: 'answer',
-        message: 'Are you sure?',
-        default: false,
+        message,
+        default: defaultChoice,
       },
     ]);
-    if (!(response as ConfirmResponse).answer) {
-      this.exit(0);
-    }
+
+    return response.answer;
   }
 
   async run() {
     const { flags } = this.parse(Destroy);
 
     if (!flags.force) {
-      await this.promptConfirmDestroy();
+      const confirmed = await this.promptConfirm('Are you sure?');
+      if (!confirmed) {
+        this.exit(0);
+      }
     }
 
     await removeTutureSuite();
 
+    const { all: allBranches } = await git.branch({ '-a': true });
+
     // Remove local tuture branch if exists.
-    const { all: allBranches } = await git.branchLocal();
     if (allBranches.includes(TUTURE_BRANCH)) {
       await git.branch(['-D', TUTURE_BRANCH]);
-      logger.log('success', 'Deleted tuture branch.');
+      logger.log('success', 'Local tuture branch has been deleted.');
+    }
+
+    const remoteBranches = allBranches.filter(
+      (branch) => branch.indexOf(TUTURE_BRANCH) > 0,
+    );
+
+    if (remoteBranches.length > 0) {
+      const confirmed = await this.promptConfirm(
+        `Do you want to delete ${remoteBranches.length} remote tuture branch(es)?`,
+      );
+
+      if (confirmed) {
+        try {
+          // Delete all remote branches.
+          await Promise.all(
+            remoteBranches.map(async (branch) => {
+              const [_, remote, ref] = branch.split('/');
+              await git.push(remote, ref, { '-d': true });
+              logger.log('success', `${branch} has been deleted.`);
+            }),
+          );
+        } catch (err) {
+          logger.log('error', err.message);
+        }
+      }
     }
 
     logger.log('success', 'Tuture tutorial has been destroyed!');
