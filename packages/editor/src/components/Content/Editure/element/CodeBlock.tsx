9e2ba49cc405fab05e32098417c19d4f53ddbe44
mRcfps <1402491442@qq.com>
2020-05-08 19:14:52 +0800

    Integrate yutang in editor

--- packages/editor/src/components/Content/Editure/element/CodeBlock.tsx
+++ packages/editor/src/components/Content/Editure/element/CodeBlock.tsx
@@ -3,6 +3,7 @@ import { Select } from 'antd';
 import { useEditure } from 'editure-react';
 import { CODE_BLOCK } from 'editure-constants';
 import { useDispatch } from 'react-redux';
+import { languages, getValidId } from 'yutang';
 
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
@@ -12,141 +13,6 @@ import { IS_MAC } from 'utils/environment';
 import { Dispatch } from 'store';
 
 import { ElementProps } from './index';
-
-const languages = [
-  'Plain Text',
-  'Bash',
-  'Basic',
-  'C',
-  'C++',
-  'C#',
-  'CSS',
-  'Dart',
-  'Diff',
-  'Django',
-  'Dockerfile',
-  'Erlang',
-  'Git',
-  'Go',
-  'GraphQL',
-  'Groovy',
-  'HTML',
-  'HTTP',
-  'Java',
-  'JavaScript',
-  'Jinja2',
-  'JSON',
-  'JSX',
-  'Kotlin',
-  'Less',
-  'Makefile',
-  'Markdown',
-  'MATLAB',
-  'Nginx',
-  'Objective-C',
-  'Pascal',
-  'Perl',
-  'PHP',
-  'PowerShell',
-  'Protobuf',
-  'Python',
-  'R',
-  'Ruby',
-  'Rust',
-  'Scala',
-  'Shell',
-  'SQL',
-  'PL/SQL',
-  'Swift',
-  'TSX',
-  'TypeScript',
-  'VB.net',
-  'Velocity',
-  'XML',
-  'YAML',
-  'LaTeX',
-  'Tcl',
-  'Verilog',
-  'Lua',
-];
-
-type PrismLangRecord = {
-  key: string;
-  value: string[];
-};
-
-const prismLangArr: PrismLangRecord[] = [
-  { key: 'text', value: ['text'] },
-  { key: 'bash', value: ['bash', 'shell'] },
-  { key: 'basic', value: ['basic'] },
-  { key: 'c', value: ['c'] },
-  { key: 'cpp', value: ['cpp'] },
-  { key: 'csharp', value: ['csharp', 'cs', 'dotnet'] },
-  { key: 'css', value: ['css'] },
-  { key: 'dart', value: ['dart'] },
-  { key: 'diff', value: ['diff'] },
-  { key: 'django', value: ['django'] },
-  { key: 'dockerfile', value: ['dockerfile', 'docker'] },
-  { key: 'erlang', value: ['erlang'] },
-  { key: 'git', value: ['git'] },
-  { key: 'go', value: ['go'] },
-  { key: 'graphql', value: ['graphql'] },
-  { key: 'groovy', value: ['groovy'] },
-  { key: 'html', value: ['html', 'xml', 'svg', 'markup', 'mathml'] },
-  { key: 'http', value: ['http'] },
-  { key: 'java', value: ['java'] },
-  { key: 'javascript', value: ['javascript', 'js'] },
-  { key: 'jinja2', value: ['jinja2'] },
-  { key: 'json', value: ['json'] },
-  { key: 'jsx', value: ['jsx'] },
-  { key: 'kotlin', value: ['kotlin'] },
-  { key: 'less', value: ['less'] },
-  { key: 'makefile', value: ['makefile'] },
-  { key: 'markdown', value: ['markdown', 'md'] },
-  { key: 'matlab', value: ['matlab'] },
-  { key: 'nginx', value: ['nginx'] },
-  { key: 'objectivec', value: ['objectivec'] },
-  { key: 'pascal', value: ['pascal', 'objectpascal'] },
-  { key: 'perl', value: ['perl'] },
-  { key: 'php', value: ['php'] },
-  { key: 'powershell', value: ['powershell'] },
-  { key: 'protobuf', value: ['protobuf'] },
-  { key: 'python', value: ['python', 'py'] },
-  { key: 'r', value: ['r'] },
-  { key: 'ruby', value: ['ruby', 'rb'] },
-  { key: 'rust', value: ['rust'] },
-  { key: 'scala', value: ['scala'] },
-  { key: 'shell', value: ['shell'] },
-  { key: 'sql', value: ['sql'] },
-  { key: 'plsql', value: ['plsql'] },
-  { key: 'swift', value: ['swift'] },
-  { key: 'tsx', value: ['tsx'] },
-  { key: 'typescript', value: ['typescript', 'ts'] },
-  { key: 'vbnet', value: ['vbnet'] },
-  { key: 'velocity', value: ['velocity'] },
-  { key: 'xml', value: ['xml'] },
-  { key: 'yaml', value: ['yaml', 'yml'] },
-  { key: 'latex', value: ['latex', 'tex', 'context'] },
-  { key: 'tcl', value: ['tcl'] },
-  { key: 'verilog', value: ['verilog'] },
-  { key: 'lua', value: ['lua'] },
-];
-
-const enumPrismLangToLanguage: Record<string, string> = {};
-
-const bimapPrismLangandLanguage = (
-  prismLangObj: PrismLangRecord,
-  index: number,
-) => {
-  const language = languages[index];
-
-  enumPrismLangToLanguage[language] = prismLangObj.key;
-  prismLangObj.value.forEach((prismLang) => {
-    enumPrismLangToLanguage[prismLang] = language;
-  });
-};
-
-prismLangArr.forEach(bimapPrismLangandLanguage);
 
 const { Option } = Select;
 
@@ -164,9 +30,6 @@ function CodeBlockElement(props: ElementProps) {
 
     editor.updateBlock(CODE_BLOCK, { lang: value });
   }
-
-  const selectValue =
-    enumPrismLangToLanguage[enumPrismLangToLanguage[lang.toLocaleLowerCase()]];
 
   const suffixIcon = (
     <IconFont type="icon-caret-down" style={{ color: 'white' }} />
@@ -189,7 +52,7 @@ function CodeBlockElement(props: ElementProps) {
       <div contentEditable={false}>
         <Select
           style={{ width: 120 }}
-          value={selectValue}
+          value={getValidId(lang)}
           onChange={handleChange}
           placeholder="选择语言"
           suffixIcon={suffixIcon}
@@ -197,9 +60,9 @@ function CodeBlockElement(props: ElementProps) {
             color: white !important;
           `}
         >
-          {languages.map((language) => (
-            <Option key={language} value={enumPrismLangToLanguage[language]}>
-              {language}
+          {Object.keys(languages).map((langId) => (
+            <Option key={languages[langId].name} value={langId}>
+              {languages[langId].name}
             </Option>
           ))}
         </Select>
