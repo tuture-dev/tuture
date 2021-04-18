8b479543fd24d34b924fd962796af2980f41c3da
pftom <1043269994@qq.com>
2021-04-18 12:42:51 +0800

    feat(editor): 添加 tailwindcss 作为基础 CSS 框架

--- packages/editor/src/main.js
+++ packages/editor/src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue';
 import { createApp, h } from 'vue-demi';
+import { Button } from 'ant-design-vue';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-import { Button } from 'ant-design-vue';
+import './index.css';
 
 Vue.config.productionTip = false;
-Vue.config.devtools = true;
 
 Vue.use(Button);
 
