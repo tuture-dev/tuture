5cdd2b1477674a253eb28ef30fdb8fa9dc243c83
pftom <1043269994@qq.com>
2021-06-15 08:22:44 +0800

    improvement(editor): 初步实现第一版本的编辑按钮,以及新建菜单逻辑

--- packages/editor/src/editor/extensions/BlockMenuTrigger.js
+++ packages/editor/src/editor/extensions/BlockMenuTrigger.js
@@ -3,7 +3,7 @@ import { Plugin } from 'prosemirror-state';
 import { isInTable } from 'prosemirror-tables';
 import { Decoration, DecorationSet } from 'prosemirror-view';
 import { Extension } from 'tiptap';
-import { findParentNode } from 'prosemirror-utils';
+import { findParentNode, findParentNodeOfType } from 'prosemirror-utils';
 import { ResolvedPos, Node } from 'prosemirror-model';
 
 const MAX_MATCH = 500;
@@ -93,37 +93,70 @@ export default class BlockMenuTrigger extends Extension {
             return false;
           },
           decorations: (state) => {
-            const parent = findParentNode((node) => {
-              return node.type.name === 'paragraph';
+            /**
+             * TODO:
+             * 两种形式：1. 最外层 2. 里层
+             * 一种间隔：外层间隔一致，里层间隔一致，整个文档的缩进每层保持一致
+             *
+             *  1. Paragraph/Blockquote/Notice/Heading  要单行，针对直接父块
+             *  2. Codeblock/Diffblock 只针对整个块
+             *  3. ul/ol/todolist 要单行，list-item/todo-item
+             *  4. Table 只针对整个块
+             *  5. Image 只针对 Image
+             */
+            console.log('schema', this.editor.schema.nodes);
+            const $from = state.selection.$from;
+            const isTopLevel = $from.depth === 1;
+
+            // 直接父块
+            const directParent = findParentNode((node) => {
+              return node.isBlock;
             })(state.selection);
 
-            const { $from } = state.selection;
-
-            const node = $from.node(1);
-            let firstParent = {
-              pos: $from.before(1),
-              start: $from.start(1),
-              depth: 1,
-              node,
-            };
-
-            if (!firstParent || !parent) {
-              return;
+            // 再往上以及父块，即直接父块的父块
+            let secondUpperParent;
+            if (!isTopLevel) {
+              const node = $from.node($from.depth - 1);
+              secondUpperParent = {
+                pos: $from.before($from.depth - 1),
+                start: $from.start($from.depth - 1),
+                depth: $from.depth - 1,
+                node,
+              };
             }
 
+            if (!directParent) return;
+
             const decorations = [];
-            const isEmpty = parent && parent.node.content.size === 0;
-            const isSlash = parent && parent.node.textContent === '/';
-            const isTopLevel = state.selection.$from.depth === 1;
+            const isEmpty = directParent.node.content.size === 0;
+            const isSlash = directParent.node.textContent === '/';
 
-            console.log('now', isEmpty, isTopLevel);
+            console.log('schema', this);
 
-            // 只有在 parent 是 paragraph，且处于 topLevel 时才可以快捷键处理
-            // TODO: 后续可以在任意符合要求的块里面使用快捷键来添加元素
-            if (parent && isTopLevel) {
+            console.log(
+              'now',
+              isEmpty,
+              isTopLevel,
+              directParent,
+              secondUpperParent,
+            );
+
+            /**
+             * 只有如下两种情况可以使用快捷键处理：
+             * 1. 鼠标在顶层，且顶层为 paragraph，且内容为空
+             * 2. 鼠标没有在最顶层，上上层为 notice/blockquote，上层为 paragraph，且内容为空
+             */
+            if (
+              (isTopLevel && directParent.node.type.name === 'paragraph') ||
+              (!isTopLevel &&
+                directParent.node.type.name === 'paragraph' &&
+                ['blockquote', 'notice'].includes(
+                  secondUpperParent.node.type.name,
+                ))
+            ) {
               if (isEmpty) {
                 decorations.push(
-                  Decoration.widget(parent.pos, () => {
+                  Decoration.widget(directParent.pos, () => {
                     button.addEventListener('click', () => {
                       this.options.onOpen('');
                     });
@@ -133,8 +166,8 @@ export default class BlockMenuTrigger extends Extension {
 
                 decorations.push(
                   Decoration.node(
-                    parent.pos,
-                    parent.pos + parent.node.nodeSize,
+                    directParent.pos,
+                    directParent.pos + directParent.node.nodeSize,
                     {
                       class: 'placeholder',
                       'data-empty-text': this.options.dictionary.newLineEmpty,
@@ -146,8 +179,8 @@ export default class BlockMenuTrigger extends Extension {
               if (isSlash) {
                 decorations.push(
                   Decoration.node(
-                    parent.pos,
-                    parent.pos + parent.node.nodeSize,
+                    directParent.pos,
+                    directParent.pos + directParent.node.nodeSize,
                     {
                       class: 'placeholder',
                       'data-empty-text': `  ${this.options.dictionary.newLineWithSlash}`,
@@ -155,25 +188,97 @@ export default class BlockMenuTrigger extends Extension {
                   ),
                 );
               }
-
-              return DecorationSet.create(state.doc, decorations);
             }
 
-            // 内容不为空时，或者处于顶层，但是并非 paragraph
-            if (firstParent && (!isEmpty || (isTopLevel && !parent))) {
+            /**
+             * 以下几种情况会弹出处理框（目前是只在最外层弹出，后续考虑类似 gitbook 可以在内层弹出）：
+             * 1. 鼠标在顶层 depth = 1，顶层为 paragraph，内容不为空
+             * 2. 鼠标在 heading 中
+             * 3. 鼠标在 notice/blockquote 中，且 directParent 为 paragraph
+             */
+            if (
+              (isTopLevel &&
+                directParent.node.type.name === 'paragraph' &&
+                directParent.node.content.size !== 0) ||
+              (secondUpperParent &&
+                directParent.node.type.name === 'paragraph' &&
+                ['notice', 'blockquote'].includes(
+                  secondUpperParent.node.type.name,
+                ) &&
+                directParent.node.content.size !== 0) ||
+              directParent.node.type.name === 'heading' ||
+              (isTopLevel &&
+                ['code_block', 'diff_block'].includes(
+                  directParent.node.type.name,
+                ))
+            ) {
               decorations.push(
-                Decoration.widget(firstParent.pos, () => {
+                Decoration.widget(directParent.pos, () => {
                   button.addEventListener('click', () => {
                     this.options.onOpen('');
                   });
                   return button;
                 }),
               );
-
-              return DecorationSet.create(state.doc, decorations);
             }
 
-            return;
+            /**
+             * 以下几种情况会弹出处理框（目前是只在最外层弹出，后续考虑类似 gitbook 可以在内层弹出）：
+             * 1. ul/ol/todolist，
+             */
+            if (
+              secondUpperParent &&
+              ['list_item', 'todo_item'].includes(
+                secondUpperParent.node.type.name,
+              )
+            ) {
+              decorations.push(
+                Decoration.widget(secondUpperParent.pos, () => {
+                  button.addEventListener('click', () => {
+                    this.options.onOpen('');
+                  });
+                  return button;
+                }),
+              );
+            }
+
+            /**
+             * 以下几种情况会弹出处理框（目前是只在最外层弹出，后续考虑类似 gitbook 可以在内层弹出）：
+             * 1. Table 针对整个块
+             */
+            const tableParent = findParentNodeOfType(
+              this.editor.schema.nodes.table,
+            )(state.selection);
+            if (tableParent) {
+              decorations.push(
+                Decoration.widget(tableParent.pos, () => {
+                  button.addEventListener('click', () => {
+                    this.options.onOpen('');
+                  });
+                  return button;
+                }),
+              );
+            }
+
+            /**
+             * 针对图片，只在图片元素上展示编辑菜单
+             */
+            const imageParent = findParentNodeOfType(
+              this.editor.schema.nodes.image,
+            )(state.selection);
+            if (imageParent) {
+              decorations.push(
+                Decoration.widget(imageParent.pos, () => {
+                  button.addEventListener('click', () => {
+                    this.options.onOpen('');
+                  });
+                  return button;
+                }),
+              );
+            }
+
+            if (decorations.length === 0) return;
+            return DecorationSet.create(state.doc, decorations);
           },
         },
       }),
